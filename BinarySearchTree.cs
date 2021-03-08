f0422543a779d32d15cb1db9f3b0fb2925abeeee
EddieMacD <eddie.macdonagh@gmail.com>
2021-03-08 16:01:56 +0000

    First version. May even work

---
+++ BinarySearchTree.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        private int Data;
+        private BinarySearchTree LeftChild;
+        private BinarySearchTree RightChild;
+
+        public BinarySearchTree(int data)
+        {
+            Data = data;
+        }
+
+        public void AddItem(int input)
+        {
+            if(input > Data)
+            {
+                if (LeftChild == null)
+                {
+                    LeftChild = new BinarySearchTree(input);
+                }
+                else
+                {
+                    LeftChild.AddItem(input);
+                }
+            }
+            else
+            {
+                if (RightChild == null)
+                {
+                    RightChild = new BinarySearchTree(input);
+                }
+                else
+                {
+                    RightChild.AddItem(input);
+                }
+            }
+        }
+
+        private void TreeTraverse (List<int> nums)
+        {
+            //Left branch traversal
+            if(LeftChild != null)
+            {
+                LeftChild.TreeTraverse(nums);
+            }
+
+            //Log current node
+            nums.Add(Data);
+
+            //Right branch traversal
+            if (RightChild != null)
+            {
+                RightChild.TreeTraverse(nums);
+            }
+        }
+
+        public int TreeCount ()
+        {
+            List<int> nums = new List<int>(0);
+            TreeTraverse(nums);
+            return nums.Count();
+        }
+
+        public void TreePrint ()
+        {
+            List<int> nums = new List<int>(0);
+            TreeTraverse(nums);
+
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                Console.Write(nums[i] + "    ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree tree = new BinarySearchTree(48);
+
+            tree.AddItem(79);
+            tree.AddItem(83);
+            tree.AddItem(82);
+            tree.AddItem(23);
+            tree.AddItem(73);
+            tree.AddItem(90);
+            tree.AddItem(8);
+            tree.AddItem(56);
+            tree.AddItem(61);
+
+            Console.WriteLine(tree.TreeCount());
+            tree.TreePrint();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            tree.AddItem(52);
+            tree.AddItem(73);
+            tree.AddItem(68);
+            tree.AddItem(85);
+            tree.AddItem(58);
+            tree.AddItem(18);
+            tree.AddItem(49);
+            tree.AddItem(47);
+            tree.AddItem(87);
+            tree.AddItem(61);
+
+            Console.WriteLine(tree.TreeCount());
+            tree.TreePrint();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            tree.AddItem(83);
+            tree.AddItem(66);
+            tree.AddItem(31);
+            tree.AddItem(64);
+            tree.AddItem(58);
+            tree.AddItem(97);
+            tree.AddItem(59);
+            tree.AddItem(10);
+            tree.AddItem(54);
+            tree.AddItem(45);
+
+            Console.WriteLine(tree.TreeCount());
+            tree.TreePrint();
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
